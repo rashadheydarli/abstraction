7cc748d867c8e54395da6416c309814346d5311d
rashadheydarli <rashadahe@code.edu.az>
2023-02-01 16:47:36 +0400

    abstraction task added

---
+++ Calculation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace test
+{
+	abstract class Calculation
+	{
+		public abstract int Operate(int number1, int number2);
+		
+	}
+	
+
+	
+	
+}
+
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace test
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var addition = new Addition();
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            int resultaddition = addition.Operate(number1, number2);
+            Console.WriteLine($"Addition: first number = {number1}, second number = {number2}, Result = {resultaddition}");
+
+
+            var substraction = new Substraction();
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            int resultsubstraction = substraction.Operate(number1, number2);
+            Console.WriteLine($"Substraction: first number={number1}, second number={number2}, Result= {resultsubstraction}");
+
+            var multiplication = new Multiplication();
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            int resultmultiplication = multiplication.Operate(number1, number2);
+            Console.WriteLine($"Multiplication : first number = {number1}, second number={number2}, Result= {resultmultiplication}");
+
+            var division = new Division();
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            int resultdivision = division.Operate(number1, number2);
+            Console.WriteLine($"Addition: first number = {number1}, second number = {number2}  , Result = {resultdivision}");
+
+        }
+    }
+}
+
+    
